77758e79819285d24fa6e50b42e5b685bc31292b
Daniel Aziegbe <danielaziegbe98@gmail.com>
2025-09-26 22:42:37 -0400

    CHANGED THE COLORS AND THE GRID

--- script.js
+++ script.js
@@ -1,95 +1,106 @@
 function add(a, b) {
-  return a + b;
+   return a + b;
 }
+
 function subtract(a, b) {
-  return a - b;
+   return a - b;
 }
+
 function multiply(a, b) {
-  return a * b;
+   return a * b;
 }
+
 function divide(a, b) {
-  if (b === 0) return "Error";
-  return a / b;
+   if (b === 0) {
+      return "ERROR";
+   }
+   return a / b;
 }
 
-function operate(operator, a, b) {
-  a = parseFloat(a);
-  b = parseFloat(b);
-  switch (operator) {
-    case "+": return add(a, b);
-    case "-": return subtract(a, b);
-    case "x": return multiply(a, b);
-    case "÷": return divide(a, b);
-    default: return b;
-  }
+function operate(a, operand, b) {
+   num1 = parseFloat(a);
+   num2 = parseFloat(b);
+   switch (operand) {
+      case "+":
+         return add(num1, num2);
+      case "-":
+         return subtract(num1, num2);
+      case "x":
+         return multiply(num1, num2);
+      case "÷":
+         return divide(num1, num2);
+      default:
+         return null;
+   }
 }
 
-const display = document.querySelector("#display");
-const previous = document.querySelector(".previous");
 const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+const previousDisplay = document.querySelector(".previous-display");
 
-let number1 = "";
-let number2 = "";
-let operator = "";
-let shouldResetDisplay = false;	
+let firstNum;
+let operator;
+let secondNum;
+let resetDisplay = false;
 
-function appendNumber(number) {
-  if (shouldResetDisplay) {
-    display.value = ''
-    shouldResetDisplay = false 
-  }
-  if (number === '.' && display.value.includes('.')) return;
-  display.value += number
+function addToDisplay(number) {
+   if (resetDisplay) {
+      display.value = "";
+      resetDisplay = false;
+   }
+   if (display.value.includes(".") && number === ".") {
+      return;
+   }
+   display.value += number;
 }
 
-function clear() {
-  number1 = "";
-  number2 = "";
-  operator = "";
-  display.value = ''
-  previous.value = ''
+function allClear() {
+   firstNum = "";
+   operator = "";
+   secondNum = "";
+   previousDisplay.value = "";
+   display.value = "";
 }
 
 function backspace() {
-  display.value = display.value.slice(0, -1)
+   display.value = display.value.slice(0, -1);
 }
 
 function getOperand(op) {
-  if (operator !== '') {
-    calculate()
-  } 
-  number1 = display.value
-  operator = op
-  previous.value = `${number1} ${op}`
-  shouldResetDisplay = true
+   if (operator) {
+      calculate();
+   }
+   firstNum = display.value;
+   operator = op;
+   previousDisplay.value = `${display.value} ${op}`;
+   resetDisplay = true;
 }
 
 function calculate() {
-  if (operator === '' || shouldResetDisplay) return
-  number2 = display.value
-  const result  = operate(operator, number1, number2)
-  display.value = (typeof result === "number") ? Math.round(result * 1000) / 1000 : result;
-  previous.value = `${number1} ${operator} ${number2} =`
-  operator = ''
-  number1 = display.value;
-  shouldResetDisplay = true;
+   if (operator === "" || resetDisplay) return;
+   secondNum = parseFloat(display.value);
+   let result = operate(firstNum, operator, secondNum);
+   display.value =
+      typeof result === "number" ? Math.round(result * 1000) / 1000 : result;
+   previousDisplay.value = `${firstNum} ${operator} ${secondNum} =`;
+   operator = "";
+   firstNum = display.value;
+   resetDisplay = true;
 }
 
-
 buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    const value = button.textContent
-
-    if (!isNaN(value) || value === '.') { 
-      appendNumber(value)
-    } else if (value === 'AC') {
-      clear()
-    } else if (value === 'DEL') {
-      backspace()
-    } else if (value === '=') {
-      calculate()
-    } else {
-      getOperand(value)
-    }
-  })
+   button.addEventListener("click", () => {
+      value = button.textContent;
+      if (!isNaN(value) || value === ".") {
+         addToDisplay(value);
+      } else if (value === "AC") {
+         allClear();
+      } else if (value === "DEL") {
+         backspace();
+      } else if (value === "=") {
+         calculate();
+      } else {
+         getOperand(value);
+      }
+   });
 });
